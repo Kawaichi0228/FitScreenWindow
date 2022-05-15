d5df5e0d64a3de15c71e30330a39d5527283447e
Kawaichi0228 <99755034+Kawaichi0228@users.noreply.github.com>
2022-05-15 14:12:20 +0900

    feat: ボタンの大きさを小さくした

--- website/src/components/ScrollTopButton.vue
+++ website/src/components/ScrollTopButton.vue
@@ -1,15 +1,15 @@
 
 <template>
-    <div id="scroll-top-button">
-        <!-- MEMO: 「href=""」を記述しないとマウスオンしてもカーソル変わらないため必要 -->
-      <a href="" @click="clickSmoothScroll('#app')"></a>
-    </div>
+  <div id="scroll-top-button">
+    <!-- MEMO: 「href=""」を記述しないとマウスオンしてもカーソル変わらないため必要 -->
+    <a href="" @click="clickSmoothScroll('#app')"></a>
+  </div>
 </template>
 
 <script>
-import smoothScroll from '../methods/SmoothScroll.vue';
+import smoothScroll from "../methods/SmoothScroll.vue";
 export default {
-  mixins: [smoothScroll] // 上記mixinを使用する宣言
+  mixins: [smoothScroll], // 上記mixinを使用する宣言
 };
 </script>
 
@@ -22,7 +22,7 @@ export default {
   background: $color-gray;
 
   /* ボタンの大きさ */
-  --page-top-width-and-height: 70px;
+  --page-top-width-and-height: 50px;
   width: var(--page-top-width-and-height);
   height: var(--page-top-width-and-height);
 
@@ -30,13 +30,13 @@ export default {
   position: fixed; /* 「ウィンドウ全体」を基準に絶対値表示指定 */
   right: 20px;
   bottom: 20px;
-  
+
   border-radius: 50%;
   opacity: 0.6;
   z-index: 1; /* レイヤー前面背面順 */
 }
 
-#scroll-top-button a{
+#scroll-top-button a {
   /* ボタンの大きさ */
   width: var(--page-top-width-and-height);
   height: var(--page-top-width-and-height);
@@ -45,20 +45,21 @@ export default {
   display: block;
   text-decoration: none;
 }
-#scroll-top-button a::before{
+
+#scroll-top-button a::before {
   color: $color-white;
 
   /* 上矢印の大きさ */
-  --page-top-uparrow: 50px;
+  --page-top-uparrow: 35px;
   font-size: var(--page-top-uparrow);
   width: var(--page-top-uparrow);
   height: var(--page-top-uparrow);
 
-  font-family: 'Font Awesome 5 Free';
+  font-family: "Font Awesome 5 Free";
   font-weight: 900;
-  content: '\f106';
+  content: "\f106";
   position: absolute;
-  top: -8px;
+  top: -5px;
   bottom: 0;
   right: 0;
   left: 0;
