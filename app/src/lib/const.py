0f042dd2e370a89344cdd8e9f3b506c034f5e603
Kawaichi0228 <99755034+Kawaichi0228@users.noreply.github.com>
2022-06-02 23:35:40 +0900

    feat: .exeのインストール先をProgramFilesにし、assetsをRomingにインストールするよう修正

--- app/src/lib/const.py
+++ app/src/lib/const.py
@@ -20,14 +20,25 @@ class MoveResizeDirection(Enum): # base number:1
 PROGRAM_NAME = "FitScreenWindow"
 
 # 各ディレクトリ
-# TODO: 実行ファイルではなく.venvがある(もしくはワークスペース？)の
-# ディレクトリ取得になってしまっている。そのため、"\app" を付加している。
-ROOT_DIR = os.getcwd() + r"\app" # 実行ファイルのパスを取得
-SOURCE_DIR = ROOT_DIR + r"\src"
-IMAGES_DIR = ROOT_DIR + r"\images"
+EXECUTION_DIR = os.getcwd() # 実行ファイルのパスを取得
+
+ROMING_DIR_BEFORE = r"C:\Users"
+USER_NAME = os.getlogin()
+ROMING_DIR_AFTER = r"AppData\Roaming"
+ROMING_DIR = f"{ROMING_DIR_BEFORE}\\{USER_NAME}\\{ROMING_DIR_AFTER}"
+
+root_directory_execution = f"{EXECUTION_DIR}\\app"
+if os.path.exists(root_directory_execution):
+    ROOT_DIR = root_directory_execution
+else:
+    root_directory_roming = f"{ROMING_DIR}\\{PROGRAM_NAME}\\app"
+    ROOT_DIR = root_directory_roming
+
+SOURCE_DIR = f"{ROOT_DIR}\\src"
+IMAGES_DIR = f"{ROOT_DIR}\\images"
 
 # faviconのパス
-FAVICON_IMAGE_PATH = IMAGES_DIR + r"\favicon.ico"
+FAVICON_IMAGE_PATH = f"{IMAGES_DIR}\\favicon.ico"
 
 # config.json(設定ファイル)のパス
-CONFIG_JSON_PATH = SOURCE_DIR + r"\config.json"
+CONFIG_JSON_PATH = f"{SOURCE_DIR}\\config.json"
